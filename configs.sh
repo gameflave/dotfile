8bd65ef8c65d5979d3a4e462e0750b7ece809f50
flave <>
2024-09-26 21:28:14 +0200

    move old config to backup instead of deleting them. improve symlink creation code

--- configs.sh
+++ configs.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
 DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-echo "
-## ###       ### ##
-Start linking paths
-## ###       ### ##
-"
+
+echo "Start linking paths"
+
 for file in "$DOTFILES_DIR/Configs"/*; do
-    if [ -d "$file" ]; then
+    if [ -e "$file" ]; then
         CONFIG_DIR="$HOME/.config/$(basename "$file")"
+
+        if [ -L $CONFIG_DIR ]; then
+            echo "  Skipping $CONFIG_DIR"
+            continue
+        fi
+
         echo "  Processing: $CONFIG_DIR"
-        if [ -d  "$CONFIG_DIR" ]; then
-            rm -r $CONFIG_DIR
+
+        if [ -e  "$CONFIG_DIR" ]; then
+            mv $CONFIG_DIR "$DOTFILES_DIR/Backups/config/"
         fi
 
         ln -s "$file" "$CONFIG_DIR"
