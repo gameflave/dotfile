307375436ef8b0d74b3e57c025e8996c21c9b8d0
flave <>
2025-06-06 13:22:58 +0200

    move im env variable fish->hyprland

--- Configs/fish/config.fish
+++ Configs/fish/config.fish
@@ -1,20 +1,19 @@
 # FIX: stop relying on cachyos config
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
-set -e GTK_IM_MODULE
-set -Ux QT_IM_MODULE fcitx
-set -Ux QT_IM_MODULES "wayland;fcitx"
 #set -Ux SDL_IM_MODULE fcitx
 set -Ux XMODIFIERS @im=fcitx
 
+# Setup ssh-agent
 if test -z "$(pgrep ssh-agent)"
   eval "$(ssh-agent -c)"
   ssh-add ~/.ssh/github
   ssh-add ~/.ssh/gitlab
 end
 
-set -Ux CC clang
-set -Ux CXX clang++
+if uwsm check may-start
+  exec uwsm start hyprland.desktop
+end
 
 # TODO: duplicate path when sourced multiple times
 set -gx PATH $PATH ~/.local/bin
